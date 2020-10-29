91a45ce088e2457ef18f1ec20acd11154257e125
Anthony Jung <junganthonyy@gmail.com>
2020-10-29 14:59:02 -0700

    Making Knight Draggable

--- src/components/Knight.js
+++ src/components/Knight.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
+
+import { ItemTypes } from './Constants';
+
+
 
 export default function Knight () {
-  return <span>♘</span>
+  const [{ isDragging }, drag] = useDrag({
+    item: {type: ItemTypes.KNIGHT},
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+  });
+
+  return (
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: 'bold',
+        cursor: 'move',
+        width: '100%',
+        height: '100%'
+      }}
+    >
+      ♘
+    </div>
+  )
 }
